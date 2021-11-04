7f3d56241a4b22cf36284d8c5aa487ee241d2375
Carla Sousa <sousa.carla09@gmail.com>
2021-11-04 12:14:45 -0300

    Login Logic

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from './src/pages/login';
 import ResetPasswd from './src/pages/resetPasswd';
 import Register from './src/pages/register';
+import mainAdopter from './src/pages/adopter/main';
+import mainNGO from './src/pages/NGO/main';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -26,7 +30,27 @@ export default function App() {
           component={ResetPasswd}
           options={{headerShown: false}}
         />
+        <Stack.Screen
+          name="MainPageAdopter"
+          component={mainAdopter}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="MainPageNGO"
+          component={mainNGO}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+function Tabs() {
+  <Tab.Navigator>
+    <Tab.Screen name="Animal Perdido" component=""/>
+    <Tab.Screen name="Favoritos" component=""/>
+    <Tab.Screen name="Principal" component=""/>
+    <Tab.Screen name="Chat" component=""/>
+    <Tab.Screen name="Perfil" component=""/>
+  </Tab.Navigator>
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,53 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../assets/styles'
 import { StyleSheet, Text, View, Image, TextInput, Linking, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+
 export default function Login() {
-  const [user, onChangeUser] = React.useState('');
-  const [passwd, onChangePasswd] = React.useState('');
+  const [user, onChangeUser] = useState('');
+  const [passwd, onChangePasswd] = useState('');
+  const [type, setType] = useState('');
+  const [message, setMessage] = useState('');
   
   const navigation = useNavigation();
-    return (
-        <View style={styles.container}>
-            <Image
-                style={style.image}
-                source={require('../assets/brand/main.png')}
-            />
-          <View style={style.infos}>
-            <Text style={style.title}>Bem vindo, por favor logue para entrar.</Text>
-          </View>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeUser}
-                value={user}
-                placeholder="Email"
-                keyboardType="default"
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangePasswd}
-                value={passwd}
-                placeholder="Senha"
-                keyboardType="default"
-            />
-            <View style={style.infos}>
-              <Text style={style.link} onPress={() => navigation.navigate('ResetPasswd')}>Esqueceu sua senha? </Text>
-              <TouchableOpacity 
-                style={styles.circledButton}
-                // onPress={}
-              >
-                <Text style={styles.textOnButton}>Login</Text>
-              </TouchableOpacity>
-              <Text style={style.title}>Não possui conta?
-                  <Text 
-                      style={style.link}
-                      onPress={() => navigation.navigate('Register')}
-                  > 
-                  Cadastra-se</Text>
-              </Text>
-            </View>
+
+  function validLogin() {
+    if (user !== '' && passwd !== '') {
+      if (user.toLowerCase() === 'ong@admin.com' && passwd === 'admin') {
+        setType('ngo')
+        setMessage('');
+      } else {
+        if (user.toLowerCase() === 'adotante@admin.com' && passwd === 'admin') {
+          setType('adopter')
+          setMessage('');
+        }
+      }
+      redirect();
+    } else {
+      setMessage('Email ou senha inválidos.');
+    }
+  }
+
+  function redirect(){
+    if (type === 'adopter') {
+      navigation.navigate('MainPageAdopter')
+    } else {
+      if (type === 'ngo') {
+        navigation.navigate('MainPageNGO')
+      }
+    }
+  }
+  return (
+      <View style={styles.container}>
+          <Image
+              style={style.image}
+              source={require('../assets/brand/main.png')}
+          />
+        <View style={style.infos}>
+          <Text style={style.title}>Bem vindo, por favor logue para entrar.</Text>
         </View>
-    );
+          <TextInput
+              style={styles.input}
+              onChangeText={onChangeUser}
+              value={user}
+              placeholder="Email"
+              keyboardType="default"
+          />
+          <TextInput
+              style={styles.input}
+              onChangeText={onChangePasswd}
+              value={passwd}
+              placeholder="Senha"
+              keyboardType="default"
+              secureTextEntry={true}
+          />
+          <View style={style.infos}>
+            <Text style={style.link} onPress={() => navigation.navigate('ResetPasswd')}>Esqueceu sua senha? </Text>
+            <Text style={styles.errorMessage}>{message}</Text>
+            <TouchableOpacity 
+              style={styles.circledButton}
+              onPress={() => validLogin()}
+            >
+              <Text style={styles.textOnButton}>Login</Text>
+            </TouchableOpacity>
+            <Text style={style.title}>Não possui conta?
+                <Text 
+                    style={style.link}
+                    onPress={() => navigation.navigate('Register')}
+                > 
+                Cadastra-se</Text>
+            </Text>
+          </View>
+      </View>
+  );
 }
 
 const style = StyleSheet.create({
